641ba3839c73694bd3a78e9a4684051331796367
Danissimo <zviagin.danila@gmail.com>
2021-09-06 10:09:11 +0300

    some fix

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,34 +4,39 @@
 document.addEventListener('DOMContentLoaded', () => {
     let hamburger = document.querySelector('.hamburger');
     let menu = document.querySelector('.menu');
-    let sidepanelArrow = document.querySelector('.sidepanel__arrow');
+    let header = document.querySelector('.promo__header');
     let sidepanel = document.querySelector('.sidepanel');
+    let sidepanelInfo = document.querySelector('.sidepanel__block');
+    let sidepanelArrow = document.querySelectorAll('.sidepanel__arrow');
+    let sidepanelText = document.querySelector('.sidepanel__text');
     let isMenuOpen = false;
     let isSidepanelOpen = false;
 
     hamburger.addEventListener('click', () => {
         if (!isMenuOpen) {
             menu.classList.add('active');
-            document.querySelectorAll('.container').forEach(item => {
-                item.classList.add('hidden');
-            });
+            sidepanel.classList.add('active');
+            header.classList.add('active');
         } else {
             menu.classList.remove('active');
-            document.querySelectorAll('.container').forEach(item => {
-                item.classList.remove('hidden');
-            });
+            sidepanel.classList.remove('active');
+            header.classList.remove('active');
         }
         isMenuOpen = !isMenuOpen;
     });
 
-    sidepanelArrow.addEventListener('click', () => {
-        if (!isSidepanelOpen) {
-            sidepanel.classList.add('active');
-            hamburger.style.display = 'none';
-        } else {
-            sidepanel.classList.remove('active');
-            hamburger.style.display = 'flex';
-        }
-        isSidepanelOpen = !isSidepanelOpen;
+    sidepanelArrow.forEach(item => {
+        item.addEventListener('click', () => {
+            if (!isSidepanelOpen) {
+                sidepanelInfo.classList.add('active');
+                sidepanelText.classList.add('active');
+                hamburger.style.display = 'none';
+            } else {
+                sidepanelInfo.classList.remove('active');
+                sidepanelText.classList.remove('active');
+                hamburger.style.display = 'flex';
+            }
+            isSidepanelOpen = !isSidepanelOpen;
+        });
     });
 });
