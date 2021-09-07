0e093acddf485686e5e7dd5e6243de38cbc76070
Danissimo <zviagin.danila@gmail.com>
2021-09-07 21:55:06 +0300

    add slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,9 +2,13 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    // menu
     let hamburger = document.querySelector('.hamburger');
     let menu = document.querySelector('.menu');
-    let header = document.querySelector('.promo__header');
+    let menuLinks = document.querySelectorAll('.menu__links');
+    let overlay = document.querySelector('.menu__overlay');
+    let header = document.querySelector('.promo__wrapper');
     let sidepanel = document.querySelector('.sidepanel');
     let sidepanelInfo = document.querySelector('.sidepanel__block');
     let sidepanelArrow = document.querySelectorAll('.sidepanel__arrow');
@@ -12,15 +16,37 @@ document.addEventListener('DOMContentLoaded', () => {
     let isMenuOpen = false;
     let isSidepanelOpen = false;
 
+    //slider
+    let sliderContainer = document.querySelector('.slider__container');
+    let position = 0;
+    let movePosition = 330;
+    let btnPrev = document.querySelector('.slider__prev');
+    let btnNext = document.querySelector('.slider__next');
+    let countSlides = document.querySelectorAll('.slider__item').length / 2;
+
+
+    // menu functions
+    menuLinks.forEach(item => {
+        item.addEventListener('click', () => {
+            menu.classList.remove('active');
+            sidepanel.classList.remove('active');
+            header.classList.remove('active');
+            overlay.classList.remove('active');
+            isMenuOpen = !isMenuOpen;
+        });
+    });
+
     hamburger.addEventListener('click', () => {
         if (!isMenuOpen) {
             menu.classList.add('active');
             sidepanel.classList.add('active');
             header.classList.add('active');
+            overlay.classList.add('active');
         } else {
             menu.classList.remove('active');
             sidepanel.classList.remove('active');
             header.classList.remove('active');
+            overlay.classList.remove('active');
         }
         isMenuOpen = !isMenuOpen;
     });
@@ -38,5 +64,29 @@ document.addEventListener('DOMContentLoaded', () => {
             }
             isSidepanelOpen = !isSidepanelOpen;
         });
+    });
+
+    // slider functions
+    btnPrev.addEventListener('click', () => {
+        if (position / movePosition == 0) {
+            position = -(movePosition * (countSlides - 3));
+        } else {
+            position += movePosition;
+        }
+        sliderContainer.style.cssText = `
+            transform: translateX(${position}px);
+        `;
+
+    });
+
+    btnNext.addEventListener('click', () => {
+        if (position <= -(movePosition * (countSlides - 3))) {
+            position = 0;
+        } else {
+            position -= movePosition;
+        }
+        sliderContainer.style.cssText = `
+            transform: translateX(${position}px);
+        `;
     });
 });
